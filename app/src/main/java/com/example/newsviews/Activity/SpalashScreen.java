03e81786d83ed7ffa1e8d851adefae3250542e05
majedul <majedict34@gmail.com>
2020-02-12 18:23:21 +0600

    Drawer Layout

--- app/src/main/java/com/example/newsviews/Activity/SpalashScreen.java
+++ app/src/main/java/com/example/newsviews/Activity/SpalashScreen.java
@@ -36,7 +36,7 @@ public class SpalashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Checking for first time launch - before calling setContentView()
+
         session = new Session(this);
         if (!session.isFirstTimeLaunch()) {
             lunchsecondaryscreen();
@@ -44,30 +44,28 @@ public class SpalashScreen extends AppCompatActivity {
         }
 
 
-        // Making notification bar transparent
+
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         setContentView(R.layout.activity_spalash_screen);
 
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-        btnSkip = (Button) findViewById(R.id.btn_skip);
-        btnNext = (Button) findViewById(R.id.btn_next);
+        viewPager =  findViewById(R.id.view_pager);
+        dotsLayout =findViewById(R.id.layoutDots);
+        btnSkip = findViewById(R.id.btn_skip);
+        btnNext =  findViewById(R.id.btn_next);
 
 
-        // layouts of all welcome sliders
-        // add few more layouts if you want
+
         layouts = new int[]{
                 R.layout.myself,
                 R.layout.newsviews,
                 R.layout.other};
 
-        // adding bottom dots
+
         addBottomDots(0);
 
-        // making notification bar transparent
         changeStatusBarColor();
 
         myViewPagerAdapter = new MyViewPagerAdapter();
@@ -84,11 +82,10 @@ public class SpalashScreen extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // checking for last page
-                // if last page home screen will be launched
+
                 int current = getItem(+1);
                 if (current < layouts.length) {
-                    // move to next screen
+
                     viewPager.setCurrentItem(current);
                 } else {
                     launchHomeScreen();
@@ -132,20 +129,20 @@ public class SpalashScreen extends AppCompatActivity {
         finish();
 
     }
-    //  viewpager change listener
+
     ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
         public void onPageSelected(int position) {
             addBottomDots(position);
 
-            // changing the next button text 'NEXT' / 'GOT IT'
+
             if (position == layouts.length - 1) {
-                // last page. make button text to GOT IT
+
                 btnNext.setText(getString(R.string.start));
                 btnSkip.setVisibility(View.GONE);
             } else {
-                // still pages are left
+
                 btnNext.setText(getString(R.string.next));
                 btnSkip.setVisibility(View.VISIBLE);
             }
--- app/src/main/java/com/example/newsviews/Api/UtilsApi.java
+++ app/src/main/java/com/example/newsviews/Api/UtilsApi.java
@@ -1,11 +1,10 @@
 package com.example.newsviews.Api;
 
-import com.example.newsviews.BuildConfig;
 
 public class UtilsApi {
 
     public static final String BASE_URL_API =  " ";
-    //public final static String BASE_URL_API = BuildConfig.BUILD_TYPE;
+
     public static BaseApiService getAPIService(){
         return RetrofitClient.getClient(BASE_URL_API).create(BaseApiService.class);
     }
